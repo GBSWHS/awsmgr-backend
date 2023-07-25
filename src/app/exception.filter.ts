d0f1c8ae3a55d5d95ff725de437ad5eb99690e27
pmh-only <opensource@pmh.codes>
2023-07-25 16:11:00 +0900

    fix : add error reporter

--- src/app/exception.filter.ts
+++ src/app/exception.filter.ts
@@ -1,12 +1,17 @@
 import { type ExceptionFilter, Catch, type ArgumentsHost, type HttpException } from '@nestjs/common'
 import { type Response } from 'express'
+import { Logger } from '../logger/logger.service'
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger()
+
   public catch (exception: HttpException, host: ArgumentsHost): void {
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<Response>()
     const status = exception.getStatus?.() ?? 500
+
+    this.logger.error(exception.message, exception)
 
     response
       .status(status)
